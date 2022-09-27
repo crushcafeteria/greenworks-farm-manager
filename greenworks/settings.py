02cc122f560de4d9fb8f89b4b18840073c149e27
Nelson Ameyo <nelson@sodium.co.ke>
2022-09-27 10:59:21 +0300

    Updating settings.py

--- greenworks/settings.py
+++ greenworks/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-ALLOWED_HOSTS = ['.sodium.co.ke','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['.sodium.co.ke','localhost','127.0.0.1','45.79.236.53']
 
 # Application definition
 
