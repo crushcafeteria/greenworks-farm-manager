473f0befb786eb8fa504a080f66fc5e5d4f4dcb8
Nelson Ameyo <nelson@sodium.co.ke>
2022-11-20 23:08:00 +0300

    Automatic deployment on Sun 20 Nov 2022 11:08:00 PM EAT

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -12,6 +12,7 @@ echo "COMPLETE => Source code successfully pushed to Git!"
 @task('deploy_on_server', ['on'=>'production'])
     cd /mnt/www/greenworks/farm-manager
     git pull origin main
+    pip install -r requirements.txt
 @endtask
 
 @story('deploy')
